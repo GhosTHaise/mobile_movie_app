a9176267d330d75b77b1b3d6a2a9c0a696e2e950
Fitiavana Sambatra <Ghostrex2@gmail.com>
2025-03-19 20:58:36 +0300

    feat : count search term

---
+++ services/appwrite.ts
@@ -0,0 +1,50 @@
+import { Client, Databases, ID, Query } from "react-native-appwrite";
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const PROJECT_ID = process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_COLLECTION_ID!;
+
+const client = new Client().setEndpoint('https://cloud.appwrite.io/v1').setProject(PROJECT_ID)
+const database = new Databases(client)
+//track the searches made by the user
+export const updateSearchCount = async (query: string, movie: Movie) => {
+    //check if a record of that search has already been stored
+    try {
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+            Query.equal('searchTerm', query)
+        ])
+
+        if (result.documents.length > 0) {
+            const existingMovie = result.documents[0]
+            console.log("🚀 ~ updateSearchCount ~ existingMovie:", existingMovie)
+
+            await database.updateDocument(
+                DATABASE_ID,
+                COLLECTION_ID,
+                existingMovie.$id,
+                {
+                    count: existingMovie.count + 1
+                }
+            )
+
+        } else {
+            await database.createDocument(
+                DATABASE_ID,
+                COLLECTION_ID,
+                ID.unique(),
+                {
+                    searchTerm: query,
+                    movie_id: movie.id,
+                    count: 1,
+                    title : movie.title,
+                    poster_url: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                }
+            )
+        }
+    } catch (error) {
+        console.log("🚀 ~ updateSearchCount ~ error:", error);
+        throw error;
+    }
+    // if a document with that search exists, increment the count
+    // else create a new document with the search and count
+}
